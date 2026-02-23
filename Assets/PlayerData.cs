94b47cc59de8abe5257aded609c280baf248058c
YasBK2 <yasmine.b.khadija@gmail.com>
2026-02-23 16:46:48 +0100

    playerdata script

---
+++ Assets/PlayerData.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+        public int ID;
+        public string Name;
+        public int Score;
+        public float ReactionTime;
+        
+        public int FoxSkinID;
+        public int HatID;
+        public int AccessoryID;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
